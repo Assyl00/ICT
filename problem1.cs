887fdc5e4683fd2175c0122c140c15dd50ffa07f
Assyl00 <78262456+Assyl00@users.noreply.github.com>
2021-02-07 00:17:51 +0600

    Add files via upload

---
+++ problem1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace problem1
+{
+    public class Solution
+    {
+        public int[] RunningSum(int[] nums)
+        {
+            for (int i = 1; i < nums.Length; i++)
+            {
+                nums[i] += nums[i - 1];
+            }
+            return nums;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            foreach(int x in s.RunningSum(new int[] { 1, 2, 3, 4, 5}))
+            Console.Write(x + " ");
+        }
+    }
+}
---
+++ problem4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace problem4
+{
+    public class Solution
+    {
+        public bool ArrayStringsAreEqual(string[] word1, string[] word2)
+        {
+            string a, b;
+            a = string.Join("", word1);
+            b = string.Join("", word2);
+            if (a == b)
+                return true;
+            else
+                return false;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            string[] word1 = { "a", "bc" };
+            string[] word2 = { "ab", "c" };
+            Console.Write(s.ArrayStringsAreEqual(word1, word2));
+           
+        }
+    }
+}
